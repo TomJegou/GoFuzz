7debfbc1ce26e3fa8c5e018c6656fc4d835ddc06
TomJegou <tomjegou10@gmail.com>
2023-12-20 08:45:25 +0100

    add activate logique

--- mock/main.go
+++ mock/main.go
@@ -22,18 +22,20 @@ var (
 			Activate: true,
 		},
 		{
-			Route:    "/robot.txt",
+			Route:    "/robot",
 			Activate: true,
 		},
 	}
 )
 
 func main() {
-	for _, route := range listRoutes {
-		http.HandleFunc(route.Route, func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Route path ok for : %s", route.Route)
-		})
+	for i, route := range listRoutes {
+		if route.Activate {
+			http.HandleFunc(route.Route, func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Route path ok for : %s %d", route.Route, i)
+			})
+		}
 	}
-	log.Printf("Mock server is up !\nListening on : http://localhost:%s", port)
+	log.Printf("Mock server is up ! 🔥\nListening on : http://localhost:%s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
